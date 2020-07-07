1ba533560d1d869de0f5e7e28c7fc4af12b4db24
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-07 15:30:25 -0300

    Componentes com Parâmetro

---
+++ src/componentes/BomDia.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+//import React, { Fragment } from 'react'
+
+// export default props => <h1>Bom dia {props.nome}!</h1>
+
+export default props =>[
+    <h1 key = 'h1'>Bom dia{props.nome}!</h1>,
+    <h2 key = 'h2'>Até Breve!</h2>
+]
+
+
+    // export default props =>
+    // <Fragment>
+    //     <h1>Bom dia {props.nome}!</h1>
+    //     <h2>Até Breve!</h2>
+    // </Fragment>
+
+
+    // export default props =>
+    // <React.Fragment>
+    //     <h1>Bom dia {props.nome}!</h1>
+    //     <h2>Até Breve!</h2>
+    // </React.Fragment>
+
+
+    // export default props =>
+    // <div>
+    //     <h1>Bom dia {props.nome}!</h1>
+    //     <h2>Até Breve!</h2>
+    // </div>
