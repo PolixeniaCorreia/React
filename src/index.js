6b1834f4b35133a194048098472a7a829d425fe0
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-07 15:46:15 -0300

    Multiplos Componentes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BomDia from './componentes/BomDia'
+import Multi from './componentes/Multiplos'
+// import BoaTarde, { BoaNoite } from './componentes/Multiplos'
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos'
 
-    ReactDOM.render(< BomDia nome = "Guilherme" />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        < Multi.BoaTarde nome='Ana' />
+        < Multi.BoaNoite nome='Bia' />
+    </div>
+    , document.getElementById('root'))
+
+// ReactDOM.render(
+//     <div>
+//         < BoaTarde nome='Ana' />
+//         < BoaNoite nome='Bia' />
+//     </div>
+//     , document.getElementById('root'))
