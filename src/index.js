4ed31c3ad7be37182c71dac4a6f70c68e634478a
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-08 12:34:43 -0300

    Componentes: Pai & Filho #2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Pai nome="Paulo" sobrenome="Santos"/>
+        <Pai nome="Paulo" sobrenome="Santos">
+            <Filho nome="Pedro" sobrenome="Santos" />
+            <Filho nome="Poli" sobrenome="Santos" />
+            <Filho nome="Carla" sobrenome="Santos" />
+        </Pai>
     </div>
     , document.getElementById('root'))
