6d10331afd1d6114cf8855f569711c97041731ba
Polixenia Correia <polixeniacorreia@gmail.com>
2020-07-08 11:13:00 -0300

    Componentes de Classe

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Multi from './componentes/Multiplos'
-// import BoaTarde, { BoaNoite } from './componentes/Multiplos'
-// import { BoaTarde, BoaNoite } from './componentes/Multiplos'
+import Saudacao from './componentes/Saudacao'
 
 ReactDOM.render(
     <div>
-        < Multi.BoaTarde nome='Ana' />
-        < Multi.BoaNoite nome='Bia' />
+        <Saudacao tipo="Bom dia" nome="Maria" />
     </div>
     , document.getElementById('root'))
-
-// ReactDOM.render(
-//     <div>
-//         < BoaTarde nome='Ana' />
-//         < BoaNoite nome='Bia' />
-//     </div>
-//     , document.getElementById('root'))
